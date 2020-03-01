4d5f9561ef870853cc2a7db0f88682dbb8f3963a
Ishan Manandhar <ishan02016@gmail.com>
2020-03-01 19:02:12 +0545

    added final build files with plugin

--- scripts/build.js
+++ scripts/build.js
@@ -1,1 +1,18 @@
-console.log('working build token file')
+const StyleDictionary = require("style-dictionary").extend({
+    source: ["src/**/*.json"],
+    platforms: {
+        sass: {
+            transformGroup: "scss",
+            buildPath: "stylesheets/",
+            files: [{
+                destination: "_typography.sass",
+                format: "scss/variables",
+                filter: {
+                    type: "type"
+                }
+            }]
+        }
+    }
+});
+
+StyleDictionary.buildAllPlatforms();
---
+++ src/ReadME.md
@@ -0,0 +1,2 @@
+# Generate styles 
+In this folder please place your generated base.JSON file and run command to build styles
